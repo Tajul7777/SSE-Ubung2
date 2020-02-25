d821e3b7903664c7288b0aaaba70e3753a2020fa
Tajul7777 <mdtajulislam7777@gmail.com>
2020-02-25 21:06:13 +0100

    Second Commit

---
+++ Tutorial2-Task2-Template/Task2-Template/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SSE.Client
+{
+    /// <summary>
+    /// Simple implementation of a TCP client.
+    /// </summary>
+    public class TcpClient
+    {
+        /// <summary>
+        /// Sends a string to a TCP endpoint, waits for an answer and returns it.
+        /// </summary>
+        /// <param name="ip">The IP address to send the request to.</param>
+        /// <param name="port">The target port to send the request to.</param>
+        /// <param name="message">The data to be sent as a string.</param>
+        /// <returns>The data received as an answer.</returns>
+        public string Request(string ip, int port, string message)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Program entry point for testing purposes.
+        /// </summary>  
+        public static void Main(string[] args)
+        {
+           // create client
+            var client = new TcpClient();
+
+            // send a request to a locally running server program             
+            var answer = client.Request("127.0.0.1", 13000, "ask NUMBER_OF_PROCESSORS\n");
+
+            // display answer and wait for user
+            Console.WriteLine("Answer: " + answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial2-Task2-Template/Task2-Template/Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SSE.Server
+{
+    public class TcpServer
+    {
+        /// <summary>
+        /// Starts the HTTP server, which keeps on running until the program is aborted.
+        /// </summary>
+        /// <param name="ip">The IP address to listen tp.</param>
+        /// <param name="port">The port to listen to.</param>
+        public void Run(string ip, int port)
+        {           
+        }
+     
+        /// <summary>
+        /// Handles an incoming line of text. 
+        /// </summary>
+        /// <param name="line">Incoming data.</param>
+        /// <returns>The answer to be sent back as a reaction to the received line or null.</returns>
+        protected virtual string ReceiveLine(string line)
+        {
+             // for testing purposes: wait for 10s and echo back received line 
+            Console.WriteLine("TCP: Simulate 10s processing time.");
+
+            // simulate 10s processing time
+            var start = DateTime.Now.Ticks;
+            while (DateTime.Now.Ticks - start < 100000000) { };
+
+            // example application: provide environment information
+            if (line.StartsWith("ask "))
+                return Environment.GetEnvironmentVariable(line.Substring(4));
+            return "What?";
+        }
+        
+        /// <summary>
+        /// Program entry point for testing purposes.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var server = new TcpServer();
+            server.Run("127.0.0.1", 13000);
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
